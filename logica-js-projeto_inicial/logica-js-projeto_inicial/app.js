59d1a46a98413104beb67383c75261d948963d51
RomuloNogueira84 <you@example.com>
2024-01-17 11:11:46 -0300

    first commit

---
+++ logica-js-projeto_inicial/logica-js-projeto_inicial/app.js
@@ -0,0 +1,10 @@
+alert('Boas vindas ao jogo do numero secreto');
+let numeroSecreto = 5;
+let chute = prompt('Escolha um número entre 1 e 10');
+
+if(numeroSecreto == chute) {
+  console.log('Isso ai! Você descobriu o numero secreto(5)')
+}else{
+  alert('você errou: ')
+}
+
